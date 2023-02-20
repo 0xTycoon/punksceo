007f5f36a5832f11295a6d66b4c68e65ca665ec9
0xTycoon <0xTycoon@users.noreply.github.com>
2023-02-20 17:38:03 +0900

    update stats

--- run.php
+++ run.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php
-
+// run report and push to github
 $dir = __DIR__;
 chdir($dir);
 `npx hardhat run {$dir}/scripts/report.js`;
-`git -C $dir commit -a -m "update stats`;
+`git -C $dir commit -a -m "update stats"`;
 `git -C $dir push origin report`;
